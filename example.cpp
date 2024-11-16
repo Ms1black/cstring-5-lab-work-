fc8aac9056d1b34b4eed0dfbfd8f5098a39ab615
Ms1black <161579194+Ms1black@users.noreply.github.com>
2024-11-16 21:57:32 +0300

    feat: upd example

--- example.cpp
+++ example.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    char s1[] = "Hello";
+    char s2[] = "Gudov";
+    
+    // Сравниваем строки s1 и s2
+    if (strcmp(s1, s2) == 0) {
+        std::cout << "Строки равны" << std::endl;
+    } else {
+        std::cout << "Строки не равны" << std::endl; // Выведется этот вариант
+    }
+    return 0;
+}
 
