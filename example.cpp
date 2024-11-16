8deeb6a7dffb0af8fccba7fd14503d1095a2a254
Ms1black <161579194+Ms1black@users.noreply.github.com>
2024-11-16 22:12:37 +0300

    feat: upd example

--- example.cpp
+++ example.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    char wisdom[] = "Зачем учиться, если инет ловит?"; // Вопрос, мучающий многих
+
+    int length = strlen(wisdom); // Считаем длину строки и сохраняем
+
+    std::cout << "Длина вопроса: " << length << " символов. Много думать не надо!" << std::endl;
+
+    return 0;
+}
 
