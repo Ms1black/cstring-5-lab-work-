ab056285b3d13f9127ad7cb41d001883dfbadb79
Ms1black <161579194+Ms1black@users.noreply.github.com>
2024-11-16 22:41:38 +0300

    feat: upd example

--- example.cpp
+++ example.cpp
@@ -1,1 +1,17 @@
+//!!!С РУССКИМИ БУКВАМИ TOLOWER НЕ РАБОТАЕТ
+#include <iostream>
+#include <cctype>
 
+int main() {
+    char question[] = "OGuzok GDE MOI YAIZA?"; // Очень громкий вопрос, требующий внимания
+    std::cout << "original: " << question << std::endl;
+
+    //пока не встретим '\0' будем поэлементно делать tolower() 
+    for (int i = 0; question[i] != '\0'; i++) {
+        question[i] = tolower(question[i]); // Успокаиваем шефа
+    }
+
+    std::cout << "spookyinii variant: " << question << std::endl; 
+
+    return 0;
+}
