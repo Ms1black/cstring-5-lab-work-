43bffda6686d0e1549e7b92a5f0c27d0f09ea803
Ms1black <161579194+Ms1black@users.noreply.github.com>
2024-11-16 22:09:09 +0300

    feat: add example

--- example.cpp
+++ example.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cstdlib>
 
+int main() {
+    char myMoney[] = "1000000"; // Мечта в виде строки
+
+    int money = atoi(myMoney); // Превращаем мечту в реальность
+
+    money *= 100; // На еду накидываем слегка
+        
+    std::cout << "Баланс: " << money  << std::endl;
+
+    return 0;
+}
