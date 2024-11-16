a35f222b8df42f4197c400136dce1cb62511c064
Ms1black <161579194+Ms1black@users.noreply.github.com>
2024-11-16 22:03:10 +0300

    feat: pizza-time

--- example.cpp
+++ example.cpp
@@ -3,12 +3,12 @@
 #include <cstring>
 
 int main() {
-    char s1[30] = "Hello";
-    char s2[] = ", Gudov!";
+    char s1[30] = "Pizza";
+    char s2[] = "s sirom!";
 
     
     strcat(s1, s2); // Присоединяю строку s2 к строке s1
 
-    std::cout << "s1: " << s1 << std::endl; // s1: Hello, Gudov!
+    std::cout << "s1: " << s1 << std::endl; // s1: Pizza s sirom
     return 0;
 }
